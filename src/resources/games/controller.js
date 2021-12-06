42c95fa6d076a72df9545b7720588906c973461c
Lana Ferrari <ferrarirangel@gmail.com>
2021-12-06 16:43:20 +0000

    Add joinGame/getGameWithUsers

--- src/resources/games/controller.js
+++ src/resources/games/controller.js
@@ -2,7 +2,7 @@ const { user } = require("../../utils/database");
 const prisma = require("../../utils/database");
 
 async function getAllGames(req, res) {
-  const userId = req.body.id;
+  const userId = req.user.id;
   try {
     const games = await prisma.games.findMany({
       where: {
@@ -16,7 +16,7 @@ async function getAllGames(req, res) {
 }
 
 async function createGame(req, res) {
-  const id = req.body.userId;
+  const id = req.user.id;
   console.log("REQ", id);
 
   try {
@@ -40,4 +40,82 @@ async function createGame(req, res) {
   }
 }
 
-module.exports = { getAllGames, createGame };
+async function getGameWithUSers(req, res) {
+  const id = parseInt(req.params.id);
+  try {
+    const game = await prisma.game.findUnique({
+      where: {
+        id,
+      },
+      include: {
+        users: {
+          select: {
+            userId: true,
+            gameId: true,
+          },
+        },
+      },
+    });
+
+    const users = game.users.map((user) => {
+      return user.userId;
+    });
+
+    delete game.users;
+    res.status(200).json({ game, users });
+  } catch (error) {
+    console.error({ error: error.message });
+    res.status(500).json({ error: error.message });
+  }
+}
+async function joinGame(req, res) {
+  const id = parseInt(req.params.id);
+  const userId = parseInt(req.user.id);
+
+  try {
+    const result = await prisma.game.update({
+      where: {
+        id,
+      },
+      data: {
+        users: {
+          create: {
+            user: {
+              connect: {
+                id: userId,
+              },
+            },
+          },
+        },
+        gameStatus: "ongoing",
+      },
+      include: {
+        users: {
+          include: {
+            user: {
+              select: {
+                id: true,
+                username: true,
+              },
+            },
+          },
+        },
+      },
+    });
+
+    const users = result.users.map((user) => {
+      return user.user;
+    });
+    res.status(200).json({ result, users });
+  } catch (error) {
+    console.error({ error: error.message });
+    res.status(500).json({ error: error.message });
+  }
+}
+
+module.exports = {
+  getAllGames,
+  createGame,
+  getGameWithUSers,
+  joinGame,
+};
--- src/resources/games/router.js
+++ src/resources/games/router.js
@@ -1,12 +1,21 @@
 const { Router, request } = require("express");
-const { protectAdmin } = require("../../utils/authentication");
+const { protectAdmin, protect } = require("../../utils/authentication");
 
-const { getAllGames, createGame } = require("./controller");
+const {
+  getAllGames,
+  createGame,
+  getGameWithUSers,
+  joinGame,
+} = require("./controller");
 
 const router = Router();
 
-router.get("/", getAllGames);
+router.get("/", protectAdmin, getAllGames);
 
-router.post("/", createGame);
+router.get("/:id", getGameWithUSers);
+
+router.patch("/:id/join", joinGame);
+
+router.post("/", protect, createGame);
 
 module.exports = router;
